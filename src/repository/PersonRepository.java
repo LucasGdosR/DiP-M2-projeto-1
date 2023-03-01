47e5a30ec3fb8d08b7d265d78bc3219e9b471cc7
LucasGdosR <109366863+LucasGdosR@users.noreply.github.com>
2023-03-01 08:42:24 -0300

    Feature: implementa repositório Wrapper com Generics

--- src/repository/PersonRepository.java
+++ src/repository/PersonRepository.java
@@ -5,6 +5,14 @@ import model.Person;
 import java.util.ArrayList;
 import java.util.List;
 
-public class PersonRepository {
-    private final List<Person> repository = new ArrayList<>();
+public class PersonRepository <E extends Person> {
+    private final List<E> repository = new ArrayList<>();
+
+    public void add(E person) {
+        repository.add(person);
+    }
+
+    public List<E> getRepository() {
+        return repository;
+    }
 }
